4bbe250a7261b8e38a55e081b0c1f5a579cc05e3
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2025-06-29 19:55:08 +0300

    Initial Commit

---
+++ PineappleJuice/PineappleJuiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PineappleJuiceApp.swift
+//  PineappleJuice
+//
+//  Created by Berkcan Tezcaner on 29.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct PineappleJuiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
